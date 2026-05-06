f26d9de16114f9e3415a5b240aad3fe6d017b60a
Mieko Chun <165838850+m1chun@users.noreply.github.com>
2026-05-06 13:52:50 -0700

    Fix errors

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 def main():
     print("Hello from dsc190-assignment5!")
 
-import os
-
-def unused_function():
-    pass
-
 def add(a, b):
     x = 1 + 2
+    return a + b
 
 if __name__ == "__main__":
     main()
